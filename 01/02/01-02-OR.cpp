b1070f7918de064b80367d9573c5e094f3491904
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2025-09-07 02:36:56 +0900

    ORパーセプトロンを実装

---
+++ 01/02/01-02-OR.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <format>
+#include "OR.h"
+
+using namespace std;
+
+int main()
+{
+	cout << format("OR(0, 0) = {}", OR(0.0, 0.0)) << endl;
+	cout << format("OR(0, 1) = {}", OR(0.0, 1.0)) << endl;
+	cout << format("OR(1, 0) = {}", OR(1.0, 0.0)) << endl;
+	cout << format("OR(1, 1) = {}", OR(1.0, 1.0)) << endl;
+
+	return 0;
+}
---
+++ 01/02/OR.h
@@ -0,0 +1,21 @@
+﻿#ifndef OR_H_INCLUDED
+#define OR_H_INCLUDED
+
+#include <array>
+#include <concepts>
+#include <numeric>
+
+template <std::floating_point T>
+T OR(T x1, T x2)
+{
+	std::array<T, 2> x = { x1, x2 };
+	constexpr std::array<T, 2> weights = { static_cast<T>(0.5), static_cast<T>(0.5) };
+	constexpr T b = static_cast<T>(-0.2);
+
+	T tmp = std::inner_product(x.begin(), x.end(), weights.begin(), T(0)) + b;
+
+	return (tmp <= 0) ? 0 : 1;
+}
+
+
+#endif //  OR_H_INCLUDED
