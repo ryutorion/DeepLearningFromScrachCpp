12b7c1512d6116009ee9b6c901fce32436c8ad6b
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2025-09-07 00:57:57 +0900

    ゼロから作るDeep Learning 01の2章のANDパーセプトロンを実装

---
+++ 01/02/01-02-AND.cpp
@@ -0,0 +1,16 @@
+﻿#include <format>
+#include <iostream>
+
+#include "AND.h"
+
+using namespace std;
+
+int main()
+{
+	cout << format("AND(0, 0) = {}", AND(0.0, 0.0)) << endl;
+	cout << format("AND(0, 1) = {}", AND(0.0, 1.0)) << endl;
+	cout << format("AND(1, 0) = {}", AND(1.0, 0.0)) << endl;
+	cout << format("AND(1, 1) = {}", AND(1.0, 1.0)) << endl;
+
+	return 0;
+}
---
+++ 01/02/AND.h
@@ -0,0 +1,17 @@
+﻿#ifndef AND_H_INCLUDED
+#define AND_H_INCLUDED
+
+#include <concepts>
+
+template <std::floating_point T>
+T AND(T x1, T x2)
+{
+	constexpr T w1 = static_cast<T>(0.5);
+	constexpr T w2 = static_cast<T>(0.5);
+	constexpr T theta = static_cast<T>(0.7);
+
+	T tmp = x1 * w1 + x2 * w2;
+	return (tmp <= theta) ? static_cast<T>(0) : static_cast<T>(1);
+}
+
+#endif // AND_H_INCLUDED
