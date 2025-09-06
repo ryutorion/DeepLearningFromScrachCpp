be21a8e33b81e3772ac54a4eff068345c7cb6e3a
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2025-09-07 02:42:10 +0900

    XORパーセプトロンを実装

---
+++ 01/02/01-02-XOR.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <format>
+#include "XOR.h"
+
+using namespace std;
+
+int main()
+{
+	cout << format("XOR(0, 0) = {}", XOR(0.0, 0.0)) << endl;
+	cout << format("XOR(0, 1) = {}", XOR(0.0, 1.0)) << endl;
+	cout << format("XOR(1, 0) = {}", XOR(1.0, 0.0)) << endl;
+	cout << format("XOR(1, 1) = {}", XOR(1.0, 1.0)) << endl;
+
+	return 0;
+}
---
+++ 01/02/XOR.h
@@ -0,0 +1,21 @@
+﻿#ifndef XOR_H_INCLUDED
+#define XOR_H_INCLUDED
+
+#include <array>
+#include <concepts>
+#include <numeric>
+
+#include "NAND.h"
+#include "OR.h"
+#include "AND.h"
+
+template <std::floating_point T>
+T XOR(T x1, T x2)
+{
+	T s1 = NAND(x1, x2);
+	T s2 = OR(x1, x2);
+	return AND(s1, s2);
+}
+
+
+#endif // XOR_H_INCLUDED
