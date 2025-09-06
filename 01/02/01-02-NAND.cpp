c3f408c42cdc990806b05d04e58097dd9fc5dcce
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2025-09-07 02:19:54 +0900

    NANDパーセプトロンを追加

---
+++ 01/02/01-02-NAND.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <format>
+#include "NAND.h"
+
+using namespace std;
+
+int main()
+{
+	cout << format("NAND(0, 0) = {}", NAND(0.0, 0.0)) << endl;
+	cout << format("NAND(0, 1) = {}", NAND(0.0, 1.0)) << endl;
+	cout << format("NAND(1, 0) = {}", NAND(1.0, 0.0)) << endl;
+	cout << format("NAND(1, 1) = {}", NAND(1.0, 1.0)) << endl;
+
+	return 0;
+}
---
+++ 01/02/NAND.h
@@ -0,0 +1,20 @@
+﻿#ifndef NAND_H_INCLUDED
+#define NAND_H_INCLUDED
+
+#include <array>
+#include <concepts>
+#include <numeric>
+
+template <std::floating_point T>
+T NAND(T x1, T x2)
+{
+	std::array<T, 2> x = { x1, x2 };
+	constexpr std::array<T, 2> weights = { static_cast<T>(-0.5), static_cast<T>(-0.5) };
+	constexpr T b = static_cast<T>(0.7);
+
+	T tmp = std::inner_product(x.begin(), x.end(), weights.begin(), T(0)) + b;
+
+	return (tmp <= 0) ? 0 : 1;
+}
+
+#endif // NAND_H_INCLUDED
